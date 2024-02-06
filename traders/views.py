02272abf282ca21cda135d62e4f1c1957e8545dd
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-02-06 15:25:48 -0300

    .

--- traders/views.py
+++ traders/views.py
@@ -3,8 +3,11 @@ from django.views.generic import CreateView
 from django.shortcuts import get_object_or_404
 from traders.models import Traders
 from traders.forms import TradersModelForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required(login_url='accounts:login'), name='dispatch')
 def home(request):
     trader = get_object_or_404(Traders, pk=id)
     return render(request, 'pages/home.html', context={
